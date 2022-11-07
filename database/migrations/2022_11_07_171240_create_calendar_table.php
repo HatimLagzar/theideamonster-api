0243e9f54cb67abd3b1584962d47febd99d250d8
Hatim Lagzar <lhatimdev@gmail.com>
2022-11-07 18:13:58 +0100

    calendar migration table

---
+++ database/migrations/2022_11_07_171240_create_calendar_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalendarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->string('basket_id');
+            $table->string('task_id');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+}
