eb5a1ceca9af1878ba0f894f4b2d018053048089
Hatim Lagzar <lhatimdev@gmail.com>
2022-11-07 18:54:54 +0100

    Calendar model extedns uuid model

--- app/Models/Calendar.php
+++ app/Models/Calendar.php
@@ -3,9 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Calendar extends Model
+class Calendar extends ModelUuid
 {
     use HasFactory;
 
